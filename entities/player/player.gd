01f158e5b4b850519c050250c5bde873a56e0ac8
Misbahur Rahman <rahman.misbah2604@gmail.com>
2026-02-06 21:35:08 +0530

    refactor(player): Update action name from 'drop' to 'pan-down'. Update 'jump_velocity' and 'speed' from constants to export variables

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
-const SPEED = 120.0
-const JUMP_VELOCITY = -350.0
-
+@export var speed : float = 120.0
+@export var jump_velocity : float = -350.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,14 +10,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction : float = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
