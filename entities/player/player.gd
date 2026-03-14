ca02fae26b300657aff9c23e10d20a1ca2d234c0
Tarexino <150453014+Tarexino@users.noreply.github.com>
2026-03-14 23:29:22 +0530

    design : added prototype blank assets (#4)
    1. Added prototype assets
    2. Added test level and adjusted player parameters for 64x64

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 @export var speed : float = 120.0
 @export var jump_velocity : float = -350.0
+@export_range(0.1, 10, 0.05) var gravity_scale : float = 1
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# v = u - gt, where
-	#	g = -gravity
+	# Add the gravity
+	# v = u - (n)gt, where
+	#   n = gravity scaling factor
+	#	g = - gravity
 	#	t = delta
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * gravity_scale * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
